b3bf7813f4d2b1b1f927c6b22b6946d47cb46b8d
f.steinhart <f.steinhart@futura-solutions.de>
2021-10-15 11:15:15 +0200

    add sample data

--- WinFormsEFSample/DataAccess/DbInitializer.cs
+++ WinFormsEFSample/DataAccess/DbInitializer.cs
@@ -45,6 +45,66 @@ namespace WinFormsEFSample.DataAccess
                 {
                     Id = new Guid("A821D32E-0D32-4E94-BB0A-75568764BF15"),
                     Name = "Estragon"
+                },
+                new()
+                {
+                    Id = new Guid("00579AF1-2D77-4C4A-96FE-570EF0455CFE"),
+                    Name = "Kerbel"
+                },
+                new()
+                {
+                    Id = new Guid("0A5923C6-266C-4478-972D-4A8DF335D658"),
+                    Name = "Knoblauch"
+                },
+                new()
+                {
+                    Id = new Guid("4858C0FB-8A43-4B61-858C-9EB684BFBDA5"),
+                    Name = "Kresse"
+                },
+                new()
+                {
+                    Id = new Guid("16C08968-1869-439A-A403-4F9757B96594"),
+                    Name = "Kümmel"
+                },
+                new()
+                {
+                    Id = new Guid("F5AF8933-3F28-4D0C-BE9F-7C86743D25A0"),
+                    Name = "Minze"
+                },
+                new()
+                {
+                    Id = new Guid("2587BD92-A90E-450B-9E78-BF0231A046DC"),
+                    Name = "Oregano"
+                },
+                new()
+                {
+                    Id = new Guid("95D6E787-81A6-435C-A6A9-2AEA905EC3D3"),
+                    Name = "Petersilie"
+                },
+                new()
+                {
+                    Id = new Guid("A6AF90E8-C94D-4D55-86E7-0E3D523AA83D"),
+                    Name = "Salbei"
+                },
+                new()
+                {
+                    Id = new Guid("0A728168-51A5-4AD7-AC8C-876AFCE21B78"),
+                    Name = "Schnittlauch"
+                },
+                new()
+                {
+                    Id = new Guid("8FC4AE53-EC57-45A5-9788-DAF495BD3111"),
+                    Name = "Thymian"
+                },
+                new()
+                {
+                    Id = new Guid("0D6E5DD3-E41D-4CD5-A639-7C0A9D69549D"),
+                    Name = "Wurzelpetersilie"
+                },
+                new()
+                {
+                    Id = new Guid("95010393-342E-4C36-90D0-8D5C9D0C13DC"),
+                    Name = "Zitronenmelisse"
                 }
             };
 
@@ -63,8 +123,141 @@ namespace WinFormsEFSample.DataAccess
                 {
                     Id = new Guid("00B2619F-9536-4CE0-9E63-780C95F3BCE5"),
                     HerbId = herbs.First(herb => herb.Name == "Basilikum").Id,
-                    From = "juni",
+                    From = "Juni",
                     To = "September"
+                },
+                new()
+                {
+                    Id = new Guid("0D3993D4-3848-4BDF-86DC-571DAD3698BC"),
+                    HerbId = herbs.First(herb => herb.Name == "Bockshornklee").Id,
+                    From = "August",
+                    To = "September"
+                },
+                new()
+                {
+                    Id = new Guid("D1B60B24-0A69-4848-AFCC-A8336E0C018C"),
+                    HerbId = herbs.First(herb => herb.Name == "Borretsch").Id,
+                    From = "Juni",
+                    To = "August"
+                },
+                new()
+                {
+                    Id = new Guid("0C60496A-80DC-422C-8A93-3555C94634FA"),
+                    HerbId = herbs.First(herb => herb.Name == "Bärlauch").Id,
+                    From = "März",
+                    To = "Juni"
+                },
+                new()
+                {
+                    Id = new Guid("6B99A058-93D3-47DA-A5F0-2A614D253754"),
+                    HerbId = herbs.First(herb => herb.Name == "Dill").Id,
+                    From = "April",
+                    To = "September"
+                },
+                new()
+                {
+                    Id = new Guid("06B9C5A0-D6D9-40F5-B4B5-BF4EBA20986A"),
+                    HerbId = herbs.First(herb => herb.Name == "Estragon").Id,
+                    From = "Mai",
+                    To = "Oktober"
+                },
+                new()
+                {
+                    Id = new Guid("3B2C6149-86EE-4444-B9B2-BB903515788D"),
+                    HerbId = herbs.First(herb => herb.Name == "Kerbel").Id,
+                    From = "April",
+                    To = "Oktober"
+                },
+                new()
+                {
+                    Id = new Guid("4EF6E92A-5165-4197-8AB3-3DFAAA056C1A"),
+                    HerbId = herbs.First(herb => herb.Name == "Knoblauch").Id,
+                    From = "Juni",
+                    To = "August"
+                },
+                new()
+                {
+                    Id = new Guid("C72EA7B9-E4C6-4A83-B1F1-22FCCF8074DF"),
+                    HerbId = herbs.First(herb => herb.Name == "Kresse").Id,
+                    From = "Januar",
+                    To = "Dezember"
+                },
+                new()
+                {
+                    Id = new Guid("2E44A90E-2A59-47B3-B071-1E45E18512F8"),
+                    HerbId = herbs.First(herb => herb.Name == "Kümmel").Id,
+                    From = "Mai",
+                    To = "Mai"
+                },
+                new()
+                {
+                    Id = new Guid("0EF4791A-DB80-4C47-8C9B-083DFFDDC014"),
+                    HerbId = herbs.First(herb => herb.Name == "Kümmel").Id,
+                    From = "August",
+                    To = "Oktober"
+                },
+                new()
+                {
+                    Id = new Guid("4E7015AE-1E7E-4393-A1C9-ED94D0AFC3A0"),
+                    HerbId = herbs.First(herb => herb.Name == "Minze").Id,
+                    From = "Mai",
+                    To = "September"
+                },
+                new()
+                {
+                    Id = new Guid("BECC6315-8D28-43DA-A16B-90925A460E13"),
+                    HerbId = herbs.First(herb => herb.Name == "Oregano").Id,
+                    From = "April",
+                    To = "Oktober"
+                },
+                new()
+                {
+                    Id = new Guid("341E37B3-6FD4-4DB2-96F3-8D90E861F4A2"),
+                    HerbId = herbs.First(herb => herb.Name == "Petersilie").Id,
+                    From = "April",
+                    To = "Oktober"
+                },
+                new()
+                {
+                    Id = new Guid("1118EE13-7D7B-4FCC-85B5-CAAEA124DAAD"),
+                    HerbId = herbs.First(herb => herb.Name == "Salbei").Id,
+                    From = "April",
+                    To = "November"
+                },
+                new()
+                {
+                    Id = new Guid("F4DB0E30-AC74-44CE-9DBD-235675C94559"),
+                    HerbId = herbs.First(herb => herb.Name == "Schnittlauch").Id,
+                    From = "April",
+                    To = "Oktober"
+                },
+                new()
+                {
+                    Id = new Guid("C9276DFD-AC0A-4A95-A3C1-DAD38F38D3AA"),
+                    HerbId = herbs.First(herb => herb.Name == "Thymian").Id,
+                    From = "März",
+                    To = "Oktober"
+                },
+                new()
+                {
+                    Id = new Guid("28A1FD0C-1AD5-4045-AA3D-73DC96235A83"),
+                    HerbId = herbs.First(herb => herb.Name == "Wurzelpetersilie").Id,
+                    From = "Januar",
+                    To = "April"
+                },
+                new()
+                {
+                    Id = new Guid("0623177E-602B-4855-90CA-34B262C343C5"),
+                    HerbId = herbs.First(herb => herb.Name == "Wurzelpetersilie").Id,
+                    From = "Oktober",
+                    To = "Dezember"
+                },
+                new()
+                {
+                    Id = new Guid("B5A6F36A-27CB-4053-9472-8F781821D4E8"),
+                    HerbId = herbs.First(herb => herb.Name == "Zitronenmelisse").Id,
+                    From = "April",
+                    To = "Oktober"
                 }
             };
 
---
+++ WinFormsEFSample/Migrations/20211015091227_InitialCreate.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WinFormsEFSample.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Herb",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Herb", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Season",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    From = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    To = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HerbId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Season", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Season_Herb_HerbId",
+                        column: x => x.HerbId,
+                        principalTable: "Herb",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Herb",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("c7660f77-9962-45ee-a601-cfa7799aa85e"), "Anis" },
+                    { new Guid("8fc4ae53-ec57-45a5-9788-daf495bd3111"), "Thymian" },
+                    { new Guid("0a728168-51a5-4ad7-ac8c-876afce21b78"), "Schnittlauch" },
+                    { new Guid("a6af90e8-c94d-4d55-86e7-0e3d523aa83d"), "Salbei" },
+                    { new Guid("95d6e787-81a6-435c-a6a9-2aea905ec3d3"), "Petersilie" },
+                    { new Guid("2587bd92-a90e-450b-9e78-bf0231a046dc"), "Oregano" },
+                    { new Guid("f5af8933-3f28-4d0c-be9f-7c86743d25a0"), "Minze" },
+                    { new Guid("16c08968-1869-439a-a403-4f9757b96594"), "Kümmel" },
+                    { new Guid("0d6e5dd3-e41d-4cd5-a639-7c0a9d69549d"), "Wurzelpetersilie" },
+                    { new Guid("4858c0fb-8a43-4b61-858c-9eb684bfbda5"), "Kresse" },
+                    { new Guid("00579af1-2d77-4c4a-96fe-570ef0455cfe"), "Kerbel" },
+                    { new Guid("a821d32e-0d32-4e94-bb0a-75568764bf15"), "Estragon" },
+                    { new Guid("4236f6f6-bb89-4952-a60e-69bf87f461ab"), "Dill" },
+                    { new Guid("c8d2c2e3-9fa0-4a85-a279-1d2e019b6cb0"), "Bärlauch" },
+                    { new Guid("2b87619d-e0c6-4271-8a53-537e95c05d7e"), "Borretsch" },
+                    { new Guid("1977d469-ef12-464c-bee6-03fc42ea1849"), "Bockshornklee" },
+                    { new Guid("32f2a89a-d70d-4ebe-8ed5-69dab23501c8"), "Basilikum" },
+                    { new Guid("0a5923c6-266c-4478-972d-4a8df335d658"), "Knoblauch" },
+                    { new Guid("95010393-342e-4c36-90d0-8d5c9d0c13dc"), "Zitronenmelisse" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Season",
+                columns: new[] { "Id", "From", "HerbId", "To" },
+                values: new object[,]
+                {
+                    { new Guid("8af96254-d025-4f5c-a56c-9b4cef659ca6"), "September", new Guid("c7660f77-9962-45ee-a601-cfa7799aa85e"), "November" },
+                    { new Guid("28a1fd0c-1ad5-4045-aa3d-73dc96235a83"), "Januar", new Guid("0d6e5dd3-e41d-4cd5-a639-7c0a9d69549d"), "April" },
+                    { new Guid("c9276dfd-ac0a-4a95-a3c1-dad38f38d3aa"), "März", new Guid("8fc4ae53-ec57-45a5-9788-daf495bd3111"), "Oktober" },
+                    { new Guid("f4db0e30-ac74-44ce-9dbd-235675c94559"), "April", new Guid("0a728168-51a5-4ad7-ac8c-876afce21b78"), "Oktober" },
+                    { new Guid("1118ee13-7d7b-4fcc-85b5-caaea124daad"), "April", new Guid("a6af90e8-c94d-4d55-86e7-0e3d523aa83d"), "November" },
+                    { new Guid("341e37b3-6fd4-4db2-96f3-8d90e861f4a2"), "April", new Guid("95d6e787-81a6-435c-a6a9-2aea905ec3d3"), "Oktober" },
+                    { new Guid("becc6315-8d28-43da-a16b-90925a460e13"), "April", new Guid("2587bd92-a90e-450b-9e78-bf0231a046dc"), "Oktober" },
+                    { new Guid("4e7015ae-1e7e-4393-a1c9-ed94d0afc3a0"), "Mai", new Guid("f5af8933-3f28-4d0c-be9f-7c86743d25a0"), "September" },
+                    { new Guid("0ef4791a-db80-4c47-8c9b-083dffddc014"), "August", new Guid("16c08968-1869-439a-a403-4f9757b96594"), "Oktober" },
+                    { new Guid("0623177e-602b-4855-90ca-34b262c343c5"), "Oktober", new Guid("0d6e5dd3-e41d-4cd5-a639-7c0a9d69549d"), "Dezember" },
+                    { new Guid("2e44a90e-2a59-47b3-b071-1e45e18512f8"), "Mai", new Guid("16c08968-1869-439a-a403-4f9757b96594"), "Mai" },
+                    { new Guid("4ef6e92a-5165-4197-8ab3-3dfaaa056c1a"), "Juni", new Guid("0a5923c6-266c-4478-972d-4a8df335d658"), "August" },
+                    { new Guid("3b2c6149-86ee-4444-b9b2-bb903515788d"), "April", new Guid("00579af1-2d77-4c4a-96fe-570ef0455cfe"), "Oktober" },
+                    { new Guid("06b9c5a0-d6d9-40f5-b4b5-bf4eba20986a"), "Mai", new Guid("a821d32e-0d32-4e94-bb0a-75568764bf15"), "Oktober" },
+                    { new Guid("6b99a058-93d3-47da-a5f0-2a614d253754"), "April", new Guid("4236f6f6-bb89-4952-a60e-69bf87f461ab"), "September" },
+                    { new Guid("0c60496a-80dc-422c-8a93-3555c94634fa"), "März", new Guid("c8d2c2e3-9fa0-4a85-a279-1d2e019b6cb0"), "Juni" },
+                    { new Guid("d1b60b24-0a69-4848-afcc-a8336e0c018c"), "Juni", new Guid("2b87619d-e0c6-4271-8a53-537e95c05d7e"), "August" },
+                    { new Guid("0d3993d4-3848-4bdf-86dc-571dad3698bc"), "August", new Guid("1977d469-ef12-464c-bee6-03fc42ea1849"), "September" },
+                    { new Guid("00b2619f-9536-4ce0-9e63-780c95f3bce5"), "Juni", new Guid("32f2a89a-d70d-4ebe-8ed5-69dab23501c8"), "September" },
+                    { new Guid("c72ea7b9-e4c6-4a83-b1f1-22fccf8074df"), "Januar", new Guid("4858c0fb-8a43-4b61-858c-9eb684bfbda5"), "Dezember" },
+                    { new Guid("b5a6f36a-27cb-4053-9472-8f781821d4e8"), "April", new Guid("95010393-342e-4c36-90d0-8d5c9d0c13dc"), "Oktober" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Season_HerbId",
+                table: "Season",
+                column: "HerbId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Season");
+
+            migrationBuilder.DropTable(
+                name: "Herb");
+        }
+    }
+}
